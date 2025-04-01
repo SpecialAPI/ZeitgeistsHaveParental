136f42a75253ce78196e2cb7eb3d1d2a2c3fdc52
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-04-01 14:05:52 +0300

    Gnaw

--- Plugin.cs
+++ Plugin.cs
@@ -9,23 +9,23 @@ namespace ZeitgeistsHaveParental
     {
         public const string GUID = "SpecialAPI.ZeitgeistsHaveParentalMod";
         public const string NAME = "Zeitgeists have Parental Mod";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.00001";
 
         public void Start()
         {
             var zeitgeist = LoadedAssetsHandler.GetEnemy("Zeitgeist_EN");
 
-            if (zeitgeist == null || zeitgeist.abilities == null || zeitgeist.abilities.Count <= 0)
+            if (zeitgeist == null || zeitgeist.abilities == null || zeitgeist.abilities.Count <= 1)
                 return;
 
-            var babyShower = zeitgeist.abilities[0];
-            zeitgeist.abilities.RemoveAt(0);
+            var gnaw = zeitgeist.abilities[1];
+            zeitgeist.abilities.RemoveAt(1);
 
             zeitgeist.passiveAbilities.Add(Passives.ParentalGenerator(new()
             {
-                ability = babyShower.ability,
+                ability = gnaw.ability,
                 cost = [Pigments.Red, Pigments.Blue, Pigments.Red, Pigments.Blue],
-                rarity = babyShower.rarity,
+                rarity = gnaw.rarity,
             }));
         }
     }
